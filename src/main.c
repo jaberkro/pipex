0b3945b47f4546f938abff54a59f664d2f3d94b0
Jorien Aberkrom <jaberkro@student.codam.nl>
2022-04-30 20:30:31 +0200

    made function to close all fd's

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: jaberkro <jaberkro@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/04/14 12:14:55 by jaberkro      #+#    #+#                 */
-/*   Updated: 2022/04/30 20:25:32 by jaberkro      ########   odam.nl         */
+/*   Updated: 2022/04/30 20:29:48 by jaberkro      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,13 +24,13 @@ void	write_exit(char *message, int exit_code)
 	exit(exit_code);
 }
 
-// void	close_fds(int *fd1, int *fd2, int *fd3, int *fd4)
-// {
-// 	close(*fd1);
-// 	close(*fd2);
-// 	close(*fd3);
-// 	close(*fd4);
-// }
+void	close_fds(t_data data)
+{
+	close(data.fd_pipe[0]);
+	close(data.fd_pipe[1]);
+	close(data.fd_in);
+	close(data.fd_out);
+}
 
 pid_t	fork_and_execute_first(t_data data, char **commands)
 {
@@ -49,10 +49,7 @@ pid_t	fork_and_execute_first(t_data data, char **commands)
 			error_exit("dup2", 1);
 		if (dup2(data.fd_pipe[1], STDOUT_FILENO) < 0)
 			error_exit("dup2", 1);
-		close(data.fd_pipe[0]);
-		close(data.fd_pipe[1]);
-		close(data.fd_in);
-		close(data.fd_out);
+		close_fds(data);
 		if (execve(path, commands, data.env) < 0)
 			error_exit("execve", 1);
 	}
@@ -76,10 +73,7 @@ pid_t	fork_and_execute_last(t_data data, char **commands)
 			error_exit("dup2", 1);
 		if (dup2(data.fd_out, STDOUT_FILENO) < 0)
 			error_exit("dup2", 1);
-		close(data.fd_pipe[0]);
-		close(data.fd_pipe[1]);
-		close(data.fd_in);
-		close(data.fd_out);
+		close_fds(data);
 		if (execve(path, commands, data.env) < 0)
 			error_exit("execve", 1);
 	}
@@ -110,10 +104,7 @@ int	main(int argc, char **argv, char **env)
 			pids[1] = fork_and_execute_last(data, commands);
 		i++;
 	}
-	close(data.fd_pipe[0]);
-	close(data.fd_pipe[1]);
-	close(data.fd_in);
-	close(data.fd_out);
+	close_fds(data);
 	waitpid(pids[0], NULL, 0);
 	waitpid(pids[1], NULL, 0);
 	return (1);
