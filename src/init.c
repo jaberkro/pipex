418b806acee1a22f8b58d694a836e8ec00ed1176
Jorien Aberkrom <jaberkro@student.codam.nl>
2022-04-30 22:44:21 +0200

    fd_out not given a variable at init

--- src/init.c
+++ src/init.c
@@ -6,7 +6,7 @@
 /*   By: jaberkro <jaberkro@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/04/14 14:10:18 by jaberkro      #+#    #+#                 */
-/*   Updated: 2022/04/30 18:32:10 by jaberkro      ########   odam.nl         */
+/*   Updated: 2022/04/30 22:37:09 by jaberkro      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,10 @@ t_data	init_data(int argc, char **argv, char **env)
 	t_data	data;
 
 	data.fd_in = open_inputfile(argv[1]);
-	data.fd_out = open_outputfile(argv[argc - 1]);
 	data.argc = argc;
 	data.argv = argv;
 	if (pipe(data.fd_pipe) < 0)
-		error_exit("pipe", 1);
+		error_exit("Pipe failed", 1);
 	data.env = env;
 	data.paths = get_paths(data.env);
 	return (data);
