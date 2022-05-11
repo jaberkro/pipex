1b6a49ad41f39849f1e3ce33242de872a119aa53
Jorien Aberkrom <jaberkro@student.codam.nl>
2022-05-11 18:45:41 +0200

    added different open_outputfile versions for normal and bonus part

--- include/pipex.h
+++ include/pipex.h
@@ -6,7 +6,7 @@
 /*   By: jaberkro <jaberkro@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/04/14 12:15:22 by jaberkro      #+#    #+#                 */
-/*   Updated: 2022/05/01 21:10:26 by jaberkro      ########   odam.nl         */
+/*   Updated: 2022/05/11 18:42:47 by jaberkro      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,6 +34,7 @@ t_data	init_data(int argc, char **argv, char **env);
 
 int		open_inputfile(char *file);
 int		open_outputfile(char *file);
+int		open_outputfile_bonus(char *file, int heredoc);
 char	**get_paths(char **env);
 char	*command_in_paths(char *argument, char **paths);
 
