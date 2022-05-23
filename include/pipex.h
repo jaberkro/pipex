c4795bb4ca686c405b8f3b2a1599521a11f300f3
Jorien Aberkrom <jaberkro@student.codam.nl>
2022-05-23 12:36:19 +0200

    added protected function prototypes

--- include/pipex.h
+++ include/pipex.h
@@ -6,7 +6,7 @@
 /*   By: jaberkro <jaberkro@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/04/14 12:15:22 by jaberkro      #+#    #+#                 */
-/*   Updated: 2022/05/21 19:14:24 by jaberkro      ########   odam.nl         */
+/*   Updated: 2022/05/23 12:10:37 by jaberkro      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,11 +35,16 @@ t_data	init_data(int argc, char **argv, char **env);
 void	command_not_found(char *argument);
 char	*command_in_paths(char *argument, char **paths);
 char	**get_paths(char **env);
-void	check_permission_infile(int fd, char *argument);
 
 void	error_exit(char *message, int exit_code);
 void	write_exit(char *message, int exit_code);
+void	write_exit_argument(char *argument, char *message, int exit_code);
 void	free_nested_array(char **to_free);
 void	close3(int fd1, int fd2, int fd3);
+
+void	protected_dup2(int writefd, int readfd);
+int		protected_fork(void);
+char	**protected_split(char *to_split, char delimiter);
+void	protected_pipe(int fd[2]);
 
 #endif
